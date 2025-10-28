6ae4f560862fcdd1f8f46f72d60470af722e9534
nulltemp <nulltemp999@gmail.com>
2025-10-28 22:27:59 +0900

    kotlin一部修正

--- local/spring-kotlin/build.gradle.kts
+++ local/spring-kotlin/build.gradle.kts
@@ -20,6 +20,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.mybatis:mybatis:3.5.11")
+	implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.4.1")
+	implementation("mysql:mysql-connector-java:8.0.30")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
